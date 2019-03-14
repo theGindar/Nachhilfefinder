3ef53663bd05cb1db20cd4c1da553024680e8a11
kevin <kevin.h@mail.de>
2019-03-14 15:12:21 +0100

    Subject.java erstellt

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/nachhilfefinder/offers/jpa/Subject.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright © 2019 Jan Leyendecker, Kevin Hartmann, Patrick Günther
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.nachhilfefinder.offers.jpa;
+
+import dhbwka.wwi.vertsys.javaee.nachhilfefinder.tasks.jpa.Task;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.TableGenerator;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Kevin Hartmann
+ */
+public class Subject implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "subject_ids")
+    @TableGenerator(name = "subject_ids", initialValue = 0, allocationSize = 50)
+    private long id;
+
+    @Column(length = 30)
+    @NotNull(message = "Das Fach darf nicht leer sein.")
+    @Size(min = 3, max = 30, message = "Das Fach muss zwischen drei und 30 Zeichen lang sein.")
+    private String name;
+
+    @OneToMany(mappedBy = "subject", fetch = FetchType.LAZY)
+    List<Task> tasks = new ArrayList<>();
+
+    //<editor-fold defaultstate="collapsed" desc="Konstruktoren">
+    public Subject() {
+    }
+
+    public Subject(String name) {
+        this.name = name;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Setter und Getter">
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+    //</editor-fold>
+
+    
+    
+}
